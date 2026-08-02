7b14f9a81aa50a6c3894636424840b13c19241ff
Mateus Thives <mateus.thives@hotmail.com>
2026-08-02 19:06:46 -0300

    Implementado e editado FuncionarioViewModels

--- controle-de-medicamentos-2026-1/ControleDeMedicamentos.WebApp/ModuloFuncionarios/FuncionarioViewModels.cs
+++ controle-de-medicamentos-2026-1/ControleDeMedicamentos.WebApp/ModuloFuncionarios/FuncionarioViewModels.cs
@@ -1,4 +1,24 @@
-namespace ControleDeMedicamentos.WebApp.ModuloFuncionarios
-{
-    public record ListarFuncionarioViewModel(int Id, string Nome, string Telefone);
-}
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeMedicamentos.WebApp.ModuloFuncionarios;
+
+public record ListarFuncionarioViewModel(int Id, string Nome, string Telefone);
+
+public record CadastrarFuncionarioViewModel(
+    string Nome,
+    string Telefone,
+    string Cpf
+);
+
+public record EditarFuncionarioViewModel(
+    int Id,
+    string Nome,
+    string Telefone,
+    string Cpf
+);
+
+public record ExcluirFuncionarioViewModel(
+    int Id,
+    string Nome
+);
